ecaf5263f58f6825bae422bcade9e9f3ab4f3fbb
RuthMeryCardenas <ruth.cardenas.perez@gmail.com>
2017-08-14 23:25:28 -0500

    Mostrando objeto en pantalla

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-//ARRAY TO OBJECT
-//Solución 1: Usando método "reduce()"
-var myArray = [['make', 'Ford'], ['model', 'Mustang'], ['year', 1964]];
+/*ARRAY TO OBJECT*/
+
+/* Solución 1: Usando método "reduce()" */
+var myArray = [['make', 'Ford'], ['model', 'Mustang'], ['year', 1964]]; //Arreglo bidimensional
 
 var myObject = myArray.reduce(function(obj, element) {
   obj[element[0]] = element[1];
@@ -8,5 +9,16 @@ var myObject = myArray.reduce(function(obj, element) {
   return obj;
 }, {});
 
-// document.write(myObject);
-console.log(myObject);
+//Mostrar en consola
+  // console.log(myObject);
+  // document.write(myObject);
+  document.write(showObject (myObject));
+  
+function showObject (myObject){
+  var result = "{<br>";
+  for (var element in myObject)
+  {
+    result += element + ": " + myObject[element]+"<br>";
+  }
+  return result + "}";
+}
