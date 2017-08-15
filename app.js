c98d8457e576e040b80bdb287abe85655d6eb1c9
RuthMeryCardenas <ruth.cardenas.perez@gmail.com>
2017-08-15 12:54:30 -0500

    Feedback 1

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
-/*ARRAY TO OBJECT*/
+// ARRAY TO OBJECT
 
-//Paso 1: Establecer valores en un arreglo bidimensional
-var array2d = [['make', 'Ford'], ['model', 'Mustang'], ['year', 1964]];
-
-//Paso 2: Convertir a objeto
-function convertToObject(myArray2d) {
+// Paso 1: Convertir a objeto
+function arrayToObject(array2d) {
   var newObject = {};
-  var limit = myArray2d.length;
-  
-  for (var i = 0; i < limit; ++i){
-    newObject[myArray2d[i][0]] = myArray2d[i][1];
+
+  for (var i = 0; i < array2d.length; i++){
+    newObject[array2d[i][0]] = array2d[i][1];
   }
+
   return newObject;
 }
 
-//Paso 3: Mostrar resultados en consola
-console.log("Input: Arreglo bidimensional a convertir");
-console.log(array2d);
-
-console.log("Output: Nuevo objeto creado");
-console.log(convertToObject(array2d));
+// Paso 2: Mostrar resultados en consola
+console.log('Input: Arreglo bidimensional a convertir');
+console.log([['make', 'Ford'], ['model', 'Mustang'], ['year', 1964]]);
+console.log('Output: Nuevo objeto creado');
+console.log(arrayToObject([['make', 'Ford'], ['model', 'Mustang'], ['year', 1964]]));
